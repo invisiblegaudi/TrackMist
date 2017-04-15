fbb7c9c3dad4d11ec022058dd4932781ba504827
John Kavanagh <john@invisiblearchitects.com>
2017-04-15 19:59:14 +0100

    validation errors

--- src/SoundCloudLogin/SoundCloudLogin.js
+++ src/SoundCloudLogin/SoundCloudLogin.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
+import styles from './SoundCloudLogin.css';
 
 class SoundCloudLogin extends Component {
 
     constructor(props) {
 	super(props);
-	this.state = { username: ''};	
-
+	this.begun = false;
+	this.state = { username: '', password: '', errors: []};	
 	this.handleChange = this.handleChange.bind(this);
 	this.handleLogin = this.handleLogin.bind(this);
     }
 
     handleChange(event) {
-	this.setState({username: event.target.value});
+	let stateChange = {};
+	this.begun = true;
+	let error = this.state.errors.indexOf(event.target.name);
+	if(event.target.value) {
+	    if(error+1) this.state.errors.splice(error,1);
+	} else {
+	    if(!error+1) this.state.errors.push(event.target.name);	    
+	}
+	stateChange[event.target.name] = event.target.value;
+	this.setState({...stateChange,...this.state});
     }
-    
+
     handleLogin() {
-	console.log('login to soundcloud');
+	if(this.begun && !this.state.errors.length) {	    
+	    console.log('login to soundcloud');	    	    
+	}
     }
     
     render(){
 	return (
 	    <div className="SoundCloudLogin">
-	    <input className="username" type="text" value={this.state.username} />
-	    <input className="password" type="password" />
+	    <p id="errors" className={this.state.errors.length ? 'show' : ''}>
+	    Please correct field(s):</p>
+	    <ul>{this.state.errors.map(function(field){
+		return <li>{field}</li>;}
+	    )}</ul>
+	    <input className={this.state.errors.includes('username') ? 'error' : ''} name="username" ref="username" type="text" onChange={this.handleChange} placeholder="Username" />
+	    <input className={this.state.errors.includes('password') ? 'error' : ''}  ref="password" name="password" type="password" onChange={this.handleChange} placeholder="password" />
 	    <button onClick={this.handleLogin}>Login to SoundCloud</button>
 	    </div>
 	);
--- src/SoundCloudLogin/SoundCloudLogin.test.js
+++ src/SoundCloudLogin/SoundCloudLogin.test.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { expect } from 'chai';
 import ReactDOM from 'react-dom';
+import {mount} from 'enzyme';
 import SoundCloudLogin from './SoundCloudLogin';
 
 it('renders without crashing', () => {
@@ -7,3 +9,15 @@ it('renders without crashing', () => {
   ReactDOM.render(<SoundCloudLogin />, input);
 });
 
+it('gives error if no password', () => {
+    const login = mount(<SoundCloudLogin />);
+    login.find('input.username').simulate('change',{username:'testuser'});
+    login.find('button').simulate('click');
+    console.log(login.props());
+    expect(login.props().error).to.equal(true);
+});
+
+it('gives error if no username', () => {
+  const input = document.createElement('input');
+  ReactDOM.render(<SoundCloudLogin />, input);
+});
