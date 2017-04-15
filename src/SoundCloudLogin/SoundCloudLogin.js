85cfa7410e466b4842a4e550806ec66dce4d06f6
John Kavanagh <john@invisiblearchitects.com>
2017-04-16 00:59:02 +0100

    UI tests

--- src/SoundCloudLogin/SoundCloudLogin.js
+++ src/SoundCloudLogin/SoundCloudLogin.js
@@ -1,43 +1,57 @@
 import React, { Component } from 'react';
 import styles from './SoundCloudLogin.css';
 
-class SoundCloudLogin extends Component {
+/* 
+ *  Login Panel for SoundCloud credentials
+ *  @author john@invisibilearchitects.com
+ * 
+*/
 
+class SoundCloudLogin extends Component {
+    
     constructor(props) {
 	super(props);
-	this.begun = false;
-	this.state = { username: '', password: '', errors: []};	
+	this.state = {
+	    username: '',
+	    password: '',
+	    errors: [ 'username','password' ],
+	    showErrors: false
+	};	
 	this.handleChange = this.handleChange.bind(this);
 	this.handleLogin = this.handleLogin.bind(this);
     }
 
     handleChange(event) {
 	let stateChange = {};
-	this.begun = true;
-	let error = this.state.errors.indexOf(event.target.name);
-	if(event.target.value) {
-	    if(error+1) this.state.errors.splice(error,1);
+	let error = this.state.errors.indexOf(event.target.name) >= 0 ? true : false; // field has error?
+	if(event.target.value.length) { // field valid if field not empty
+	    if(error) this.state.errors.splice(error,1); // remove error for that field
 	} else {
-	    if(!error+1) this.state.errors.push(event.target.name);	    
+	    if(!error) this.state.errors.push(event.target.name); // add error for that field
 	}
-	stateChange[event.target.name] = event.target.value;
-	this.setState({...stateChange,...this.state});
+	console.log(event.target.name,event.target.value);
+	stateChange[event.target.name] = event.target.value; // update field value
+	this.setState({...stateChange,...this.state}); // update state
     }
 
     handleLogin() {
-	if(this.begun && !this.state.errors.length) {	    
-	    console.log('login to soundcloud');	    	    
+	if(!this.state.errors.length) {
+	    // TODO: call to backend
+	    console.log('login to soundcloud');    
+	} else {
+	    this.setState({...this.state,...{showErrors: true}});
 	}
     }
     
     render(){
 	return (
 	    <div className="SoundCloudLogin">
-	    <p id="errors" className={this.state.errors.length ? 'show' : ''}>
-	    Please correct field(s):</p>
-	    <ul>{this.state.errors.map(function(field){
-		return <li>{field}</li>;}
+	    <div id="errors" className={ this.state.errors.length && this.state.showErrors ? 'show' : ''}>
+	    <p>Please correct field(s):</p>
+	    <ul id="errorList">{this.state.errors.map((field) => {
+		return <li key={field.toString()}>{field}</li>;}
 	    )}</ul>
+	    </div>
 	    <input className={this.state.errors.includes('username') ? 'error' : ''} name="username" ref="username" type="text" onChange={this.handleChange} placeholder="Username" />
 	    <input className={this.state.errors.includes('password') ? 'error' : ''}  ref="password" name="password" type="password" onChange={this.handleChange} placeholder="password" />
 	    <button onClick={this.handleLogin}>Login to SoundCloud</button>
--- src/SoundCloudLogin/SoundCloudLogin.test.js
+++ src/SoundCloudLogin/SoundCloudLogin.test.js
@@ -11,13 +11,20 @@ it('renders without crashing', () => {
 
 it('gives error if no password', () => {
     const login = mount(<SoundCloudLogin />);
-    login.find('input.username').simulate('change',{username:'testuser'});
+    login.ref('username').simulate('change',{target: {name:'username',value:'testuser'}});
     login.find('button').simulate('click');
-    console.log(login.props());
-    expect(login.props().error).to.equal(true);
+    expect(login.find('#errorList').children()).to.have.length(1);
 });
 
-it('gives error if no username', () => {
-  const input = document.createElement('input');
-  ReactDOM.render(<SoundCloudLogin />, input);
+ it('gives error if no username', () => {
+    const login = mount(<SoundCloudLogin />);
+    login.find('input[name="password"]').simulate('change',{target:{name:'password',value:'testpass'}});
+    login.find('button').simulate('click');
+    expect(login.find('#errorList').children()).to.have.length(1);
+});
+
+it('gives errorList if nothing entered', () => {
+    const login = mount(<SoundCloudLogin />);
+    login.find('button').simulate('click');
+    expect(login.find('#errorList').children()).to.have.length(2);
 });
