3742d0fa8237176a9a59440b1e03c896103b58e5
John Kavanagh <john@invisiblearchitects.com>
2017-04-15 13:13:53 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>TrackMist</h2>
+        </div>
+        <p className="App-intro">
+          To get started, login to your SoundCloud account
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
