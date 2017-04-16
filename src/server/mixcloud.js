40e69e356f3f627e52236f130db4d0c454c728d1
John Kavanagh <john@invisiblearchitects.com>
2017-04-16 17:51:46 +0100

    test get tracklist for mix

--- src/server/mixcloud.js
+++ src/server/mixcloud.js
@@ -1,25 +1,42 @@
 'use strict';
 
 const request = require('request');
-const http = require('http');
 
 const location = {
     protocol: 'https:',
-    hostname: 'www.mixcloud.com',
-    pathname: '/BBCEssentialMix/helena-hauff-essential-mix-25022017/'
-    };
+    baseUrl: 'www.mixcloud.com'
+};	
 
-    request({
-        "uri": "/player/details",
-        "baseUrl": location.protocol + "//" + location.hostname,
-        "qs": { "key": location.pathname },
-        "json": true
-    }, (error, response, data) => {
-        if (!error && response.statusCode === 200 && data.cloudcast.sections.length > 0) {
-            data.cloudcast.sections.forEach((track)=>{
-		console.log(track.artist,track.title);
-	    })
-        } else {
-            throw new Error(error);
-        }
+
+const getTracklist = function(mixPath){
+
+    return new Promise((resolve,reject) => {
+	
+	request({
+	    "uri": "/player/details",
+	    "baseUrl": location.protocol + "//" + location.hostname,
+	    "qs": { "key": mixPath },
+	    "json": true
+	}, (error, response, data) => {
+	    if (!error && response.statusCode === 200 && data.cloudcast.sections.length > 0) {
+		data.cloudcast.sections.forEach((track)=>{
+		    tracklist.push({artist:track.artist,title:track.title});
+ 		}).then(()=>{
+		   resolve(tracklist); 
+		});
+	    } else {
+		reject(new Error(error));
+	    }
+	});
+	
     });
+}
+
+(async () => {
+    try {
+	let tracklist = await getTracklist();
+	console.log(tracklist);
+    } catch(e) {
+	console.log(error);
+    }
+});
